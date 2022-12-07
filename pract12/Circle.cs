5f6a490df93d5d9e9a547d1e6bf5ae9e17b51ad1
KramSany <friman.kop@bk.ru>
2022-12-07 12:13:26 +0300

    g

--- pract12/Circle.cs
+++ pract12/Circle.cs
@@ -10,34 +10,30 @@ namespace pract12
 {
     class CircleWithTwoRadius
     {
-        private double _radius1;
-        private double _radius2;
-        private double _midle;
-        public double Midle { get { return _midle; } set { _midle = value; } }
-        public double Radius1 { get { return _radius1; } set { _radius1 = value; } }
+        private double _radius;
 
-        public double Radius2 { get { return _radius2; } set { _radius2 = value; } }
-        public CircleWithTwoRadius(double midle, double radius1, double radius2)
+
+        public double Radius { get { return _radius; } set { _radius = value; } }
+
+
+        public CircleWithTwoRadius(double radius)
         {
-            if (radius1 < radius2) throw new ArgumentException("First radius wasn't the biggest than second");
-            _midle = midle;
-            _radius1 = radius1;
-            _radius2 = radius2;
+            if (radius < 0) throw new ArgumentException("You are input value less 0");
+            _radius = radius;
         }
-        public void AreaAllRinga(CircleWithTwoRadius circle2)
+        public string AreaAllRinga(CircleWithTwoRadius circle2)
         {
-            double[] ringa = new double[3];
-            double ring1 = 3.14 * Radius1 * Radius1;
-            double ring2 = 3.14 * circle2.Radius2 * circle2.Radius2;
+            if (_radius < circle2._radius) throw new ArgumentException("First radius was above second");
+            double ring1 = 3.14 * Radius * Radius;
+            double ring2 = 3.14 * circle2.Radius * circle2.Radius;
             double ringGlobal = ring1 - ring2;
-            ringa =  new[] { ring1, ring2, ringGlobal };
-            MessageBox.Show($"S1 = {ring1}\n S2 = {ring2}\n S3 = {ringGlobal}");
+            return ($"S1 = {ring1}\nS2 = {ring2}\nS3 = {ringGlobal}");
 
-            
+
         }
         public override string ToString()
         {
-            return $"{Midle},{Radius1},{Radius2}";
+            return $"{Radius}";
         }
     }
 }
--- pract12/MainWindow.xaml.cs
+++ pract12/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace pract12
 {
@@ -20,46 +22,56 @@ namespace pract12
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+        CircleWithTwoRadius circle1;
+        CircleWithTwoRadius circle;
+
         public MainWindow()
         {
-
-            //CircleWithTwoRadius circle1 = new (5,8,7);
-            //CircleWithTwoRadius circle = new(5, 7, 4);
-            FindCentury find = new(1567);
-            MessageBox.Show(find.Find().ToString());
             InitializeComponent();
-            //circle.AreaAllRinga(circle1);
-
         }
 
         private void Exit_Click(object sender, RoutedEventArgs e)
         {
-
+            Close();
         }
 
         private void Result_Click(object sender, RoutedEventArgs e)
         {
-             
+            circle = new(int.Parse(FirstRadiusTB.Text));
+            circle1 = new(Int32.Parse(SecondRadiusTB.Text));
+            resultTBCircle.Text = (circle1.AreaAllRinga(circle).ToString());
         }
 
         private void About_Click(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Создатель: Поливода А.А\nЗадание:Даны два круга с общим центром и радиусами R1 и R2 (R1 > R2). " +
+                "Найти площади \r\nэтих кругов S1 и S2, а также площадь S3 кольца, внешний радиус которого равен \r\nR1, " +
+                "внутренний радиус равен R2: S1 = PI*(R1)2\r\n, S2 = PI*(R2)2\r\n, S3 = S1 – S2. В\r\nкачестве значения PI использовать" +
+                " 3.14.\r\n Дан номер некоторого года (целое положительное число). Определить " +
+                "\r\nсоответствующий ему номер столетия, учитывая, что, к примеру, началом 20 \r\nстолетия был 1901 год.");
         }
 
         private void ResultCenuty_Click(object sender, RoutedEventArgs e)
         {
-            MessageBoxResult result;
-            result = MessageBox.Show("maybe u don't want?","Вопрос", MessageBoxButton.YesNo);
-            if (result == MessageBoxResult.Yes)
-            {
-                for (int i = 0; i < 10; i++)
-                {
-                    MessageBox.Show("u are gey"); 
-                }
-            }
-            
+            FindCentury find1 = new(Int32.Parse(YearTB.Text));
+            resultTBFindCentury.Text = (find1.Find().ToString());
+        }
+
+        DispatcherTimer timer;
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            timer = new DispatcherTimer();
+            timer.Tick += Timer_Tick;
+            timer.Interval = new TimeSpan(0, 0, 0, 1, 0);
+            timer.IsEnabled = true;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            DateTime dateTime = DateTime.Now;
+            tbWatch.Text = dateTime.ToString("hh:mm:ss");
+            tbDate.Text = dateTime.ToString("dd.MM.yyyy");
+            tbTask.Text = "Практическая 12. Вариант 8";
         }
     }
 }
